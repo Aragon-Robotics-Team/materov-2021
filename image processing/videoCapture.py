43cc459e4ced46d5dd1604075ff34dab2382992e
vfan888 <59328308+vfan888@users.noreply.github.com>
2021-11-09 17:46:42 -0800

    wrote videoCapture file

--- image processing/videoCapture.py
+++ image processing/videoCapture.py
@@ -1,13 +1,32 @@
+import numpy as np
+import argparse
+import imutils
 import cv2
+import keyboard
 
-videoStreamObject = cv2.VideoCapture(0)
+
+videoCaptureObject = cv2.VideoCapture(0)
+#if we want cam on
 result = True
-while(result):
-    #when space is pressed
-    if (cv2.waitKey(1)):
-        #read the image from the camera
-        ret, frame = videoCaptureObject.read()
-        #show the iamge
-        cv2.imshow("NewPicture.jpg", frame)
-        #stop loop
+snapshots = ["front.jpg", "back.jpg", "top.jpg", "left.jpg", "right.jpg"]
+i = 0;
+#for name in snapshots:
+    while(result):
+        ret,frame = videoCaptureObject.read()
+        cv2.imshow("Capturing Video",frame)
         result = False
+        if keyboard.is_pressed("q"):
+            if i < len(snapshots)
+            cv2.imwrite(snapshots[i],frame)
+            cv2.imshow(snapshots[i], frame)
+            i++
+
+
+    videoCaptureObject.release()
+    cv2.waitKey(0)
+    while(result):
+        cv2.waitKey(0)
+        ret,frame = videoCaptureObject.read()
+        cv2.imwrite(name,frame)
+        result = False
+    videoCaptureObject.release()
