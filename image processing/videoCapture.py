b4c7caacb9b06bcb3c2fdeaf5c9f01b0b7e7e149
vfan888 <59328308+vfan888@users.noreply.github.com>
2021-11-09 17:47:52 -0800

    new commit for videoCapture but i saved this time :)

--- image processing/videoCapture.py
+++ image processing/videoCapture.py
@@ -1,32 +1,32 @@
+#shows video feed and takes snapshots (+ saves them to prenamed files)
 import numpy as np
 import argparse
 import imutils
 import cv2
 import keyboard
+import time
 
 
 videoCaptureObject = cv2.VideoCapture(0)
-#if we want cam on
 result = True
+
 snapshots = ["front.jpg", "back.jpg", "top.jpg", "left.jpg", "right.jpg"]
 i = 0;
-#for name in snapshots:
-    while(result):
-        ret,frame = videoCaptureObject.read()
-        cv2.imshow("Capturing Video",frame)
-        result = False
-        if keyboard.is_pressed("q"):
-            if i < len(snapshots)
+while(result):
+    ret,frame = videoCaptureObject.read()
+    cv2.imshow("Capturing Video",frame)
+    #deletes every frame as the next one comes on, closes all windows when q is pressed
+    if(cv2.waitKey(1) & 0xFF == ord('q')):
+        videoCaptureObject.release()
+        cv2.destroyAllWindows()
+    #when s is pressed
+    if keyboard.is_pressed('s'):
+        #and the index is less than the length of the snapshot list
+        if i < len(snapshots):
+            #take as snapshot, save it, show it
             cv2.imwrite(snapshots[i],frame)
             cv2.imshow(snapshots[i], frame)
-            i++
-
-
-    videoCaptureObject.release()
-    cv2.waitKey(0)
-    while(result):
-        cv2.waitKey(0)
-        ret,frame = videoCaptureObject.read()
-        cv2.imwrite(name,frame)
-        result = False
-    videoCaptureObject.release()
+            i += 1
+            time.sleep(1)
+        else:
+            result=False
