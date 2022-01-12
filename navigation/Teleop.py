913fd7705ecaa134e1efea660e84915a356906ab
Sannie Wan <sanniewan@gmail.com>
2022-01-11 20:45:46 -0800

    Teleop with 2 thrusters lul

--- navigation/Teleop.py
+++ navigation/Teleop.py
@@ -6,11 +6,12 @@ from time import sleep
 
 #global in a function will be visible to the whole program
 deadband = 0.2  # axis value must be greater than this number
-arduino = serial.Serial(port='/dev/cu.usbmodem141101', baudrate=115200, timeout=.1)
+arduino = serial.Serial(port='/dev/cu.usbmodem142101', baudrate=115200, timeout=.1)
 LH = 0 #Left horizontal axis
 LV = 1 #Left vertical axis
 
-turn = 500
+turnconstant = 300
+forwardconstant = 300
 thrustermiddle = 1500
 
 
@@ -30,22 +31,38 @@ def init():
 
 def loop():
     while True:
-        pygame.event.pump()
-        HAxis = j.get_axis(LH)
-        if abs(HAxis) > deadband:
-            print(HAxis)
-            thrustervalue = int(HAxis * turn + thrustermiddle)  # cast to integer
-            print('Thruster value: ' + str(thrustervalue))
-            value = write_read(str(thrustervalue))
-            print('Arduino reads: ' + value)  # printing the value
-            pygame.event.clear() # clears the queue so it doesn't get overloaded...?
+                pygame.event.pump()
+                HAxis = j.get_axis(LH) #get joystick values
+                VAxis = j.get_axis(LV)
+                if abs(HAxis) > deadband or abs(VAxis) > deadband: #check for deadband fulfillment
+                    print('x-axis: ' + str(HAxis))
+                    print('y-axis: ' + str(VAxis))
+                    turn1 = HAxis * turnconstant
+                    forward1 = VAxis * forwardconstant
+                    turn2 = HAxis * turnconstant
+                    forward2 = VAxis * turnconstant
+                    #calculating thruster speeds
+                    thrustervalue1 = int(thrustermiddle - forward1 + turn1)  # cast to integer
+                    thrustervalue2 = int(thrustermiddle - forward2 - turn2)
 
-        sleep(0.3)  # wait 0.3 seconds so the terminal dont go crazy
+                    print('Thruster value: ' + str(thrustervalue1) + ',' + str(thrustervalue2))
+
+                    #have arduino read your values and say it back to make sure its getting them
+                    value = write_read(str(thrustervalue1) + ',' + str(thrustervalue2) + ',')
+
+                    # you can write any string as long as you change the respective settings on the arduino side as well
+                    # for example, thrustervalue1 + ',' + thrustervalue2
+                    # and on the arduino side, read string until ',' or something
+
+                    print('Arduino reads: ' + value)  # printing the value to make sure values match up with previously printed values
+                    pygame.event.clear() # clears the queue so it doesn't get overloaded...?
+
+                    sleep(0.1)  # wait 0.3 seconds so the terminal dont go crazy
 
 def write_read(x):
     arduino.write(bytes(x, 'utf-8'))
     time.sleep(0.05)
-    data = arduino.readline().decode('utf-8').rstrip() # rstrip removes whitespace decode decodes, surprisingly
+    data = arduino.readline().decode('utf-8').rstrip()   # rstrip removes whitespace decode decodes, surprisingly
     return data
 
 
