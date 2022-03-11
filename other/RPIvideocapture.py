375e430dc9a7b93251d062ac653339ab704e5189
vfan888 <59328308+vfan888@users.noreply.github.com>
2022-03-10 17:21:01 -0800

    added rpi video capture code

---
+++ other/RPIvideocapture.py
@@ -0,0 +1,19 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
+
+time.sleep(0.1)
+
+result = True
+
+while result:
+    camera.capture(rawCapture, format = "bgr")
+    image = rawCapture.PiRGBArray
+    cv2.imshow("Image", image)
+    # if (cv2.waitKey(1) & 0xFF == ord('q')):
+    #     videoCaptureObject.release()
+    #     cv2.destroyAllWindows()
