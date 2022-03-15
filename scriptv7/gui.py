7fc8ee3a7f008bc6b4f91343bcac5001a9600696
vfan888 <59328308+vfan888@users.noreply.github.com>
2022-03-15 16:35:11 -0700

    asdf

---
+++ scriptv7/gui.py
@@ -0,0 +1,155 @@
+from tkinter import *
+import glob
+from img_proc.measure_fishes import measureFish
+from img_proc.measure_fishes import averageLength
+
+from img_proc.measure_fishes import ValuesAndCalc
+
+import cv2
+import multiprocessing
+
+from img_proc.photomosaic import cropping
+from img_proc.photomosaic import resize_image
+from img_proc.photomosaic import stitch
+import time
+
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+
+camera = PiCamera()
+
+#SETUP ------------------------------------------------------------------------------------------------------
+root = Tk()
+root.geometry("1300x750")
+
+#TEST ------------------------------------------------------------------------------------------------------
+def asdf():
+    print("hello")
+
+btn = Button(root, text = "hello", command = asdf)
+btn.grid(row = 0, column = 1, sticky = 'e')
+
+#PHOTOMOSAIC ------------------------------------------------------------------------------------------------------
+# def startPhotomosaic():
+#     print("asdf")
+#     glob.photomosaicCount = 0
+#     print("Starting photomosaic")
+#     print("Type s 4 times to take snapshots, and one more time to construct the photomosaic")
+#     print("Type q to quit the photomosaic")
+#     glob.photomosaicVideo = True #switches video feed to photomosaic Video
+#
+# btn = Button(root, text = "Start Photomosaic", command = startPhotomosaic)
+# btn.grid(row = 1,column = 1, sticky = 'e')
+#
+startPhotomosaic = False
+photomosaicCount = 0
+
+def beginPhotomosaic():
+    global startPhotomosaic
+    print("Photomosaic begun")
+    print("Click take photomosaic button to take each snapshot")
+    startPhotomosaic = True
+    photomosaicCount = 0
+
+btn = Button(root, text = "Start Photomosaic", command = beginPhotomosaic)
+btn.grid(row = 1, column = 1, sticky = 'e')
+
+def takePhotomosaicPhoto():
+    global startPhotomosaic
+    global photomosaicCount
+    if startPhotomosaic == True:
+        if photomosaicCount < len(glob.snapshots):
+            frame = PiRGBArray(camera)
+            cv2.imwrite(glob.snapshots[glob.photomosaicCount], frame)
+            cropping(glob.snapshots[glob.photomosaicCount])
+            center_height = cv2.imread(glob.snapshots[0]).shape[0]
+            center_width = cv2.imread(glob.snapshots[0]).shape[1]
+            width_ratio = center_width/cv2.imread(glob.snapshots[glob.photomosaicCount]).shape[1]
+            height_ratio = center_height/cv2.imread(glob.snapshots[glob.photomosaicCount]).shape[0]
+            if photomosaicCount < 3:
+                resized = resize_image(cv2.imread(glob.snapshots[glob.photomosaicCount]), width_ratio, width_ratio)
+            else:
+                resized = resize_image(cv2.imread(glob.snapshots[glob.photomosaicCount]), height_ratio, height_ratio)
+
+            cv2.imwrite(glob.snapshots[photomosaicCount], resized)
+            print("Snapshot #" + str(photomosaicCount) + " taken")
+            #cv2.imshow(glob.snapshots[i], frame)
+            time.sleep(1)
+            photomosaicCount += 1
+        else:
+            print("All photomosaic snapshots taken")
+            stitch()
+            startPhotomosaic = False
+    else:
+        print("Photomosaic has not been started yet")
+
+btn = Button(root, text = "Take Photomosaic Snapshot", command = takePhotomosaicPhoto)
+btn.grid(row = 2, column = 1, sticky = 'e')
+
+def resetPhotomosaic():
+    global startPhotomosaic
+    global photomosaicCount
+    startPhotomosaic = False
+    photomosaicCount = 0
+
+btn = Button(root, text = "Reset Photomosaic Snapshot", command = resetPhotomosaic)
+btn.grid(row = 3, column = 1, sticky = 'e')
+
+
+#MEASURE FISHIES ------------------------------------------------------------------------------------------------------
+fishCount = 0
+def start_measure_fish():
+    global fishCount
+    frame = PiRGBArray(camera)
+    if fishCount < 3:
+        measureFish(frame)
+        fishCount = fishCount + 1
+    else:
+        averageFishLength = averageLength()
+        ValuesAndCalc(averageFishLength)
+
+label = Label(root, text = "(Click to 3 times to take photos and calculate)", font = 10)
+label.grid(row = 5, column = 1, sticky = 'n')
+
+btn = Button(root, text="Measure Fish", command = start_measure_fish)
+btn.grid(row = 4,column = 1, sticky = 'e')
+
+def resetMeasureFish():
+    print("Measuring Fish Task Reset")
+    glob.allFishLengths = [0,0,0]
+    glob.countfishCoords = 0
+    glob.fishPictureCount = 0
+
+btn = Button(root, text="Reset Fish Measuring", command = resetMeasureFish)
+btn.grid(row = 6, column = 1, sticky = 'e')
+
+#DRIFT
+from misc.drift import floatLocation
+btn = Button(root, text = "Calculate Float Location", command = floatLocation)
+btn.grid(row = 7, column = 1, sticky = 'e')
+
+#VIDEO FEED ------------------------------------------------------------------------------------------------------
+#pretty much the same lag as when the video feed is in the loop
+from PIL import Image, ImageTk
+
+# Create a Label to capture the Video frames
+label = Label(root, height = 700, width = 1000)
+label.grid(row = 0, column = 0, rowspan = 30)
+
+# Define function to show frame
+def show_frames():
+   # Get the latest frame and convert into Image
+   cv2image= cv2.cvtColor(cap.read()[1],cv2.COLOR_BGR2RGB)
+   img = Image.fromarray(cv2image)
+   # Convert image to PhotoImage
+   imgtk = ImageTk.PhotoImage(image = img)
+   label.imgtk = imgtk
+   label.configure(image=imgtk)
+   # Repeat after an interval to capture continiously
+   label.after(20, show_frames)
+
+show_frames()
+
+def updateGUI():
+    while True:
+        root.update()
---
+++ scriptv7/img_proc/gui.py
@@ -0,0 +1,77 @@
+def appStart(input_q, output_q, fish_q):
+    import tkinter as tk
+    import cv2
+    from img_proc import measure_fishes
+    from img_proc.measure_fishes import averageLength
+    from img_proc.measure_fishes import measureFish
+    from PIL import Image, ImageTk
+    from time import sleep
+    import threading
+
+    input_queue = input_q
+    output_queue = output_q
+    fish_queue = fish_q
+
+    videoCaptureObject = cv2.VideoCapture(0)
+    class Application(tk.Frame):
+        def __init__(self, master = None):
+            print("init")
+            tk.Frame.__init__(self,master)
+            print("fish count")
+            # self.grid()
+            self.fishCount = 0
+            print("setup")
+
+            self.root = tk.Tk()
+            self.root.geometry("1300x750")
+            #self.setup()
+
+            #MEASURE FISHIES ------------------------------------------------------------------------------------------------------
+
+            self.label = tk.Label(self.root, text = "(Click to 3 times to take photos and calculate)", font = 10)
+            self.label.grid(row = 3, column = 1, sticky = 'n')
+
+            self.btn = tk.Button(self.root, text="Measure Fish", command = self.start_measure_fish)
+            self.btn.grid(row = 2,column = 1, sticky = 'e')
+
+            self.vid_label = tk.Label(self.root, height = 700, width = 1000)
+            self.vid_label.grid(row = 0, column = 0, rowspan = 30)
+
+            # video_thread = threading.Thread(target = self.show_frames)
+            # video_thread.start()
+            self.show_frames()
+
+        #PHOTOMOSAIC ------------------------------------------------------------------------------------------------------
+
+        #MEASURE FISHIES ------------------------------------------------------------------------------------------------------
+        fishCount = 0
+        def start_measure_fish(self):
+            ret, frame = videoCaptureObject.read()
+            if Application.fishCount < 3:
+                measureFish(frame)
+                Application.fishCount = Application.fishCount + 1
+            else:
+                print("calculating average length")
+                averageFishLength = averageLength()
+                fish_q.put(averageFishLength)
+                print("put in queue")
+
+        def resetMeasureFish(self):
+            measure_fishes.resetFish()
+
+        #VIDEO FEED ------------------------------------------------------------------------------------------------------
+        def show_frames(self):
+           # Get the latest frame and convert into Image
+           cv2image= cv2.cvtColor(videoCaptureObject.read()[1],cv2.COLOR_BGR2RGB)
+           img = Image.fromarray(cv2image)
+           # Convert image to PhotoImage
+           imgtk = ImageTk.PhotoImage(master = self.root, image = img)
+           self.vid_label.imgtk = imgtk
+           self.vid_label.configure(image=imgtk)
+           # Repeat after an interval to capture continiously
+           self.vid_label.after(10, self.show_frames)
+
+    app = Application()
+
+    while True:
+        app.update()
