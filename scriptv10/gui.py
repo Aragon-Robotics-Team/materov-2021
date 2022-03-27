23466b1f14c3f4ba36d85c56162d193c9ec08f18
Martin <mz14128915@outlook.com>
2022-03-27 11:05:30 -0700

    e
    e

--- scriptv10/gui.py
+++ scriptv10/gui.py
@@ -25,6 +25,80 @@ def asdf():
 btn = Button(root, text = "hello", command = asdf)
 btn.grid(row = 0, column = 1, sticky = 'e')
 
+#TIMER ------------------------------------------------------------------------------------------------------
+
+minute=StringVar()
+second=StringVar()
+hours=StringVar()
+
+sec = StringVar()
+mins= StringVar()
+hrs= StringVar()
+
+# #  # #
+
+tk.Entry(root, textvariable = sec, width = 2, font = 'arial 12').place(x=200, y=155) # Seconds
+tk.Entry(root, textvariable = mins, width =2, font = 'arial 12').place(x=175, y=155) # Mins
+tk.Entry(root, textvariable = hrs, width =2, font = 'arial 12').place(x=150, y=155) # Hours
+
+# #  # #
+
+minute.set('00')
+second.set('00')
+hours.set('00')
+
+sec.set('00')
+mins.set('00')
+hrs.set('00')
+times = 0
+
+# #  # #
+
+def countdown():
+    global times
+    print("hello") #testing, working but code not working
+    times = int(hrs.get())*3600+ int(mins.get())*60 + int(sec.get())
+    while times > 0:
+        minute,second = (times // 60 , times % 60)
+
+        hour = 0
+        if minute > 60:
+            hour , minute = (minute // 60 , minute % 60)
+
+        sec.set(second)
+        mins.set(minute)
+        hrs.set(hour)
+
+        root.update()
+        time.sleep(1)
+        times -= 1
+        if(times == 0):
+            sec.set('00')
+            mins.set('00')
+            hrs.set('00')
+            return
+
+
+tk.Button(root, text='START', bd ='5', command = countdown, bg = 'white', font = 'arial 10 bold').place(x=150, y=210)
+# #  # #
+
+def stop():
+    global times
+    minute.set('00')
+    second.set('00')
+    hours.set('00')
+    sec.set('00')
+    mins.set('00')
+    hrs.set('00')
+    times = 0
+    #root.destroy()
+    #python = sys.executable
+    #os.execl(python, python, * sys.argv)
+
+
+
+tk.Button(root, text='STOP', bd ='5', command = stop, bg = 'white', font = 'arial 10 bold').place(x=150, y=250)
+
 #PHOTOMOSAIC ------------------------------------------------------------------------------------------------------
 # def startPhotomosaic():
 #     print("asdf")
