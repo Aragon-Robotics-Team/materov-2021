13a9a8a6cca59a5daef1eafdacf8ea53506263d2
Sannie Wan <sanniewan@gmail.com>
2022-03-28 15:44:13 -0700

    Pass queue objects into Config class as instance variables

--- scriptv10/nav/teleop.py
+++ scriptv10/nav/teleop.py
@@ -1,19 +1,19 @@
 import nav.teleopConfig1
-# from nav.teleopConfig1 import Config  # on rpi, DELETE newnavigation part
+from nav.teleopConfig1 import Config  # on rpi, DELETE scriptv10 part
 from nav.tracer import agg
 
-def teleopMain(in_queue, out_queue):
-    # robot = Config('Mac', True, False, input_queue, output_queue) # comp type, serial on, serial recieve on
-    # #robot = teleopConfig1.Config('Mac', True, False)
-    # robot.joy_init()
 
-    input_queue = in_queue
-    output_queue = out_queue
+def teleopMain(input_queue, output_queue):
+    # robot = Config('Mac', True, False, input_queue, output_queue) # comp type, serial on, serial recieve on, input, output queue
+
     # statuses = [0, 0, 0, 0, 1, 1, 1, 1]
     # output_queue.put(statuses)
     # print("put in queue")
 
-    nav.teleopConfig1.teleop_1(input_queue, output_queue)
+    robot = Config('Mac', True, False, input_queue, output_queue)
+    robot.joy_init()
+
+
 
 if __name__ == '__main__':
     teleopMain()
