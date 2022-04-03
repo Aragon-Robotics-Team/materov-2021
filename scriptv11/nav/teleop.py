acc3ff4f86ad0b2468fc651a6852d02fe1c5badb
Sannie Wan <sanniewan@gmail.com>
2022-04-03 09:35:32 -0700

    scriptv11

---
+++ scriptv11/nav/teleop.py
@@ -0,0 +1,19 @@
+from nav.teleopConfig1 import Config  # RPI IMPORTS
+# from scriptv10.nav.teleopConfig1 import Config # mac imports
+
+def teleopMain(input_queue, output_queue):
+    # robot = Config('Mac', True, False, input_queue, output_queue) # comp type, serial on, serial recieve on, input, output queue
+
+    # statuses = [0, 0, 0, 0, 1, 1, 1, 1]
+    # output_queue.put(statuses)
+    # print("put in queue")
+
+    robot = Config('Mac', True, False, input_queue, output_queue)
+    robot.joy_init()
+
+
+
+if __name__ == '__main__':
+    teleopMain()
+
+#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
