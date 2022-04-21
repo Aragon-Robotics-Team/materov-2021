d76ebeeb6a0b07fdfb7d50f3f3b0d62e83706db8
iceninja3 <98727663+iceninja3@users.noreply.github.com>
2022-04-20 22:39:49 -0700

    Update ItWorks.ino

--- ItWorks.ino
+++ ItWorks.ino
@@ -55,3 +55,42 @@ void loop() {
 }
 
 //At testing check: If buoyancy time is enough(for it to sink and then float back up)
+
+
+//Testing Code: 
+
+/* 
+
+
+//NTS: Make sure code reflects which pins on the arduino use the pins
+
+
+void delaySec(int s){
+  for(int i=0; i<10*s; i++){
+    delay(1000);
+  }
+} //delaying by the second
+
+
+
+
+//NTS: Calculate amount of time necessary for syringes to empty/fill up
+//Found to be 20 seconds
+
+void setup() {
+  pinMode (IN1, OUTPUT);
+  pinMode (IN2, OUTPUT);
+
+
+  //delayMin(5); // delay for float to be put into appropriate spot(setup+carrying to product demo area
+}
+
+void loop() {
+  digitalWrite(IN1, HIGH); //5V
+  digitalWrite(IN2, LOW); //Ground
+  delay(5*1000);
+  digitalWrite(IN1, LOW); //5V
+  digitalWrite(IN2, HIGH); 
+  delay(5*1000);
+}
+*/
