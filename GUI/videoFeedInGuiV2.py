4195947d673517769c5b8d42f164d41e76f20321
sanniewan <sanniewan@gmail.com>
2022-04-21 16:41:10 -0700

    cam

--- GUI/videoFeedInGuiV2.py
+++ GUI/videoFeedInGuiV2.py
@@ -1,15 +1,17 @@
 
 # Define function to show frame
 def show_frames():
+   print("show frames")
    # Get the latest frame and convert into Image
    cv2image= cv2.cvtColor(cap.read()[1],cv2.COLOR_BGR2RGB)
+#    video = v4l2capture.Video_device("/dev/video0")
    img = Image.fromarray(cv2image)
    # Convert image to PhotoImage
    imgtk = ImageTk.PhotoImage(image = img)
    label.imgtk = imgtk
    label.configure(image=imgtk)
    # Repeat after an interval to capture continiously
-   label.after(20, show_frames)
+   label.after(1, show_frames)
 
 if __name__ == "__main__":
 
@@ -17,6 +19,8 @@ if __name__ == "__main__":
    from tkinter import *
    from PIL import Image, ImageTk
    import cv2
+   import select
+   from time import sleep
 
    # Create an instance of TKinter Window or frame
    win = Tk()
@@ -26,12 +30,14 @@ if __name__ == "__main__":
 
    # Create a Label to capture the Video frames
    label = Label(win, height=700, width=700)
-   label.place(x=700, y=0)
+   label.pack()
    cap = cv2.VideoCapture(0)
+#    sleep(5)
+#    print("showing image")
+#    ret, frame = cap.read()
+#    cv2.imshow("hello", frame)
+#    cv2.waitKey()
 
-
+   print("starting show frames")
    show_frames()
-
-
-   while True:
-       win.update()
+   win.mainloop()
